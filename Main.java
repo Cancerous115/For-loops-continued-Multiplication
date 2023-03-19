af58e8ff7ea99dbc90942ce59d445459a1e89bd4
Cancerous115 <107626947+Cancerous115@users.noreply.github.com>
2023-03-19 16:30:28 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main{
+	public static void main(String[]args){
+		int number=5;
+		for(int multiplier=1;multiplier< 10;multiplier++){
+			//prints everything in a single line. we can put \n to change that 
+			System.out.printf("%d X %d=%d \n",number,multiplier,number * multiplier);
+		}
+	}
+}
